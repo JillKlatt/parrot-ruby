4f641b8bab568734d7ce9d26793f094bedbd8da8
JillKlatt <klatt.jill@gmail.com>
2021-02-01 23:17:42 -0600

    commit tree 8e95bf5620594159ca493e1793f2313635f02c41

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+parrot = "Squawk!"
+def parrot(chirp="Squawk!")
+   puts "#{chirp}"
+   return "#{chirp}"
+end
+ parrot()
